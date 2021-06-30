c03084757b7d2a8f0423891a576dbd467d8493a0
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-07-01 01:04:07 +0900

    [WIP]implement trace package

---
+++ trace/tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+    "bytes"
+    "testing"
+)
+
+func TestNew(t *testing.T) {
+    var buf bytes.Buffer
+    tracer := New(&buf)
+    if tracer == nil {
+	t.Error("tracer is nil")
+    } else {
+	tracer.Trace("hello, tracer package")
+	if buf.String() != "hello, tracer package\n" {
+	    t.Errorf("'%s' bad string output", buf.String())
+	}
+    }
+}
